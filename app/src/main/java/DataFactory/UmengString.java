4aac802725ee91738b504e0cb9c57cdc666bf8df
David Lin <linxiangpeng92@gmail.com>
2015-08-22 16:52:49 +0800

    update add UmengString

---
+++ app/src/main/java/DataFactory/UmengString.java
@@ -0,0 +1,33 @@
+package DataFactory;
+
+import android.content.Context;
+
+/**
+ * Created by david on 15-8-22.
+ * <li> 用于管理友盟统计点
+ * <li> 所有的统计点统一通过该类来管理
+ */
+public class UmengString {
+
+    private Context context;
+    // 所有统计点的字段在此定义
+    public final String CHECKUPDATA = "check_updata";
+    public final String DOWNLOADAPPLICTION = "download_appliction";
+    public final String GEUSTSESSION = "geust_session";
+    public final String LOGINERROR = "login_error";
+    public final String SHAREAPPLICTION = "share_appliction";
+    public final String SHOWDEVLOPER = "show_devloper";
+    public final String SHOWHELP = "show_help";
+    public final String VISITERROR = "visit_error";
+    public final String VISITOK = "visit_ok";
+    public final String VISITHOME = "visit_home";
+    public final String VISITSCORE = "visit_score";
+    public final String VISITTABLE = "visit_table";
+    public final String VISITEXAM = "visit_exam";
+    public final String VISITCOURSE = "visit_course";
+    public final String VISITSIANUP = "visit_signup";
+    public final String VISITAPPRAISE = "visit_appraise";
+    public final String VISITCALENDAR = "visit_calendar";
+    public final String VISITSETTINGS = "visit_settings";
+    public final String CLEARDATA = "clear_data";
+}
